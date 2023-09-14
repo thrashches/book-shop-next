e506578d9f7656d2a7e4626b2e003e54025071d5
thrashches <thrashches>
2023-09-14 12:08:08 +0300

    Fix slider motions bug

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,7 @@
 'use client';
 import styles from "./Slider.module.scss";
 import {useEffect, useRef, useState} from "react";
-import {motion, AnimatePresence} from "framer-motion";
+import {motion} from "framer-motion";
 import First from "../../../public/slider/1.png";
 import Second from "../../../public/slider/2.png";
 import Third from "../../../public/slider/3.png";
@@ -35,21 +35,16 @@ export default function Slider() {
     return (
         <section className={styles.Slider}>
             <div className={styles.Slider__container}>
-                <AnimatePresence
-                    initial={true}
-                    mode={'wait'}
-                >
-                    <motion.div
-                        className={styles.Slider__img}
-                        style={{
-                            backgroundImage: `url(${items[index].src})`
-                        }}
-                        key={index}
-                        initial={{x: 300, opacity: 0}}
-                        animate={{x: 0, opacity: 1}}
-                        exit={{x: -300, opacity: 0}}
-                    />
-                </AnimatePresence>
+                <motion.div
+                    className={styles.Slider__img}
+                    style={{
+                        backgroundImage: `url(${items[index].src})`
+                    }}
+                    key={index}
+                    initial={{x: 300, opacity: 0}}
+                    animate={{x: 0, opacity: 1}}
+                    exit={{x: -300, opacity: 0}}
+                />
             </div>
 
             <div className={styles.Slider__controls}>
